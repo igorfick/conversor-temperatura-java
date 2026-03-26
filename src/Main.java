f07076074d22f382320bb7e6ea118aadebcba8fa
Igor de Assis Coutinho <igorassis290501@gmail.com>
2026-03-26 20:08:53 -0300

    feat: adiciona conversao de Celsius para Fahrenheit e Kelvin

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,19 @@
+import java.util.Scanner;
 
 public class Main {
 	
 	public static void main(String[] args) {
 		
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Digite a temperatura em Celsius: ");
+		double celsius = sc.nextDouble();
+		double fahrenheit = (celsius * 9 / 5 ) + 32;
+		double kelvin = celsius + 273.15;
 		
+		System.out.println("Temperatura em Celsius: " + celsius);
+		System.out.println("Temperatura em Fahrenheit: " + fahrenheit);
+		System.out.println("Temperatura em Kelvin: " + kelvin);
 		
+		sc.close();		
 	}
 }
