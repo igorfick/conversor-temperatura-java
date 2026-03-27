9e912859be80fa12835c06d06bb67fca98234012
Igor de Assis Coutinho <igorassis290501@gmail.com>
2026-03-26 21:07:23 -0300

    refactor: separa logica de conversao em metodos

--- src/Main.java
+++ src/Main.java
@@ -7,13 +7,20 @@ public class Main {
 		Scanner sc = new Scanner(System.in);
 		System.out.print("Digite a temperatura em Celsius: ");
 		double celsius = sc.nextDouble();
-		double fahrenheit = (celsius * 9 / 5 ) + 32;
-		double kelvin = celsius + 273.15;
+		double fahrenheit = converterParaFahrenheit(celsius);
+		double kelvin = converterParaKelvin(celsius);
 		
 		System.out.println("Temperatura em Celsius: " + celsius);
 		System.out.println("Temperatura em Fahrenheit: " + fahrenheit);
 		System.out.println("Temperatura em Kelvin: " + kelvin);
 		
 		sc.close();		
+	}
+	
+	public static double converterParaFahrenheit(double celsius) {
+		return (celsius * 9 / 5) + 32;
+	}
+	public static double converterParaKelvin(double celsius) {
+		return celsius + 273.15;
 	}
 }
